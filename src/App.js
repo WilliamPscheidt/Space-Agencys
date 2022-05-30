13f6b3ae6ebb6577b6cffb70fa7599a37c0b6266
William Pscheidt <williampscheidt@hotmail.com>
2022-05-30 00:13:14 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-import ExibirDados from "./Componentes/ExibirDados";
-
 import './Estilos/Global.css'
 
 function App() {
-  
+
   const [pagina, setPagina] = useState(0)
   const [dados, setDados] = useState([])
 
@@ -15,19 +13,19 @@ function App() {
   }, [])
 
   const IncrementarDados = () => {
-    setPagina(pagina+10)
-    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset="+pagina)
-    .then(res => {
-      setDados(res.data.results)
-    })
+    setPagina(pagina + 10)
+    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset=" + pagina)
+      .then(res => {
+        setDados(res.data.results)
+      })
   }
 
   const DecrementarDados = () => {
-    setPagina(pagina-10)
-    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset="+pagina)
-    .then(res => {
-      setDados(res.data.results)
-    })
+    setPagina(pagina - 10)
+    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset=" + pagina)
+      .then(res => {
+        setDados(res.data.results)
+      })
   }
 
   console.log(dados)
@@ -38,9 +36,28 @@ function App() {
       <button onClick={() => DecrementarDados()}>Página-</button>
       <div className="container">
 
-        <ExibirDados
-          agencia = {dados.length>0 ? dados : "Sem dados"}
-        />
+        {
+          dados.length > 0 ?
+            dados.map((item, i) => {
+              return [
+                <div className='card'>
+                  <span>{dados[i].abbrev}</span>
+                  <span>{dados[i].url}</span>
+                  <span>{dados[i].name}</span>
+                  <span>{dados[i].type}</span>
+                  <span>{dados[i].country_code}</span>
+                  <span>{dados[i].description}</span>
+                  <span>{dados[i].administrator}</span>
+                  <span>{dados[i].founding_year}</span>
+                  <span>{dados[i].launchers}</span>
+                  <span>{dados[i].spacecraft}</span>
+                  <span>{dados[i].image_url}</span>
+                </div>
+              ]
+            })
+            :
+            "Oi"
+        }
       </div>
 
 
--- src/Componentes/ExibirDados.js
+++ src/Componentes/ExibirDados.js
@@ -3,44 +3,24 @@ import React from 'react'
 const ExibirDados = (props) => {
   return (
     <>
+
+        {
+            props.agencia.map((item, i) => {
+                console.log(i)
+            })
+        }
         <div className='card'>
             <span>{props.agencia[0].abbrev}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agencia[1].abbrev}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaC}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaD}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaE}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaF}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaG}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaH}</span>
-        </div>
-
-        <div className='card'>
-            <span>{props.agenciaI}</span>
-        </div>
-        
-        <div className='card'>
-            <span>{props.agenciaJ}</span>
+            <span>{props.agencia[0].url}</span>
+            <span>{props.agencia[0].name}</span>
+            <span>{props.agencia[0].type}</span>
+            <span>{props.agencia[0].country_code}</span>
+            <span>{props.agencia[0].description}</span>
+            <span>{props.agencia[0].administrator}</span>
+            <span>{props.agencia[0].founding_year}</span>
+            <span>{props.agencia[0].launchers}</span>
+            <span>{props.agencia[0].spacecraft}</span>
+            <span>{props.agencia[0].image_url}</span>
         </div>
     </>
   )
