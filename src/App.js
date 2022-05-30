b98406c14a6cf22116b2fb7d23baa25da7a90ad4
William Pscheidt <williampscheidt@hotmail.com>
2022-05-29 23:39:11 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import ExibirDados from "./Componentes/ExibirDados";
+
+import './Estilos/Global.css'
 
 function App() {
+  
+  const [pagina, setPagina] = useState(0)
+  const [dados, setDados] = useState([])
+
+  useEffect(() => {
+    IncrementarDados()
+  }, [])
+
+  const IncrementarDados = () => {
+    setPagina(pagina+10)
+    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset="+pagina)
+    .then(res => {
+      setDados(res.data.results)
+    })
+  }
+
+  const DecrementarDados = () => {
+    setPagina(pagina-10)
+    axios.get("https://spacelaunchnow.me/api/ll/2.1.0/agencies/?offset="+pagina)
+    .then(res => {
+      setDados(res.data.results)
+    })
+  }
+
+  console.log(dados)
+
   return (
     <div className="App">
-      teste
+      <button onClick={() => IncrementarDados()}>Página+</button>
+      <button onClick={() => DecrementarDados()}>Página-</button>
+
+      <ExibirDados
+        agenciaA = {dados.length>0 ? dados[0].abbrev : "Sem dados"}
+        agenciaB = {dados.length>0 ? dados[1].abbrev : "Sem dados"}
+        agenciaC = {dados.length>0 ? dados[2].abbrev : "Sem dados"}
+        agenciaD = {dados.length>0 ? dados[3].abbrev : "Sem dados"}
+        agenciaE = {dados.length>0 ? dados[4].abbrev : "Sem dados"}
+        agenciaF = {dados.length>0 ? dados[5].abbrev : "Sem dados"}
+        agenciaG = {dados.length>0 ? dados[6].abbrev : "Sem dados"}
+        agenciaH = {dados.length>0 ? dados[7].abbrev : "Sem dados"}
+        agenciaI = {dados.length>0 ? dados[8].abbrev : "Sem dados"}
+        agenciaJ = {dados.length>0 ? dados[9].abbrev : "Sem dados"}
+      />
+
     </div>
   );
 }
---
+++ src/Componentes/ExibirDados.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ExibirDados = (props) => {
+  return (
+    <>
+        <div><span>{props.agenciaA}</span></div>
+        <div><span>{props.agenciaB}</span></div>
+        <div><span>{props.agenciaC}</span></div>
+        <div><span>{props.agenciaD}</span></div>
+        <div><span>{props.agenciaE}</span></div>
+        <div><span>{props.agenciaF}</span></div>
+        <div><span>{props.agenciaG}</span></div>
+        <div><span>{props.agenciaH}</span></div>
+        <div><span>{props.agenciaI}</span></div>
+        <div><span>{props.agenciaJ}</span></div>   
+    </>
+  )
+}
+
+export default ExibirDados
